f83ee732066fb0eda41a394b59c03ac1c2bad809
Francesco Randi <francesco.randi@gmail.com>
2019-09-06 16:57:16 -0400

    changes to setup.py to compile on tigressdata (c++11 not the default one). though it doesn't run on tigressdata

--- setup.py
+++ setup.py
@@ -11,9 +11,11 @@ _dsmm_c = Extension('wormbrain.reg._dsmm_c',
                         '/home/francesco/.local/lib/eigen',
 						'../eigen',
 						'../boost',
+						'/projects/LEIFER/lib/include/eigen',
+						'/projects/LEIFER/lib/include/boost',
                         numpy.get_include()
                         ],
-                    extra_compile_args=['-ffast-math','-O3'])
+                    extra_compile_args=['-ffast-math','-O3','-std=c++11'])
 
 setup(name='wormbrain',
       version='0.1',
