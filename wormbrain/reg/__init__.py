d28cd2fc7e33287054e782bfa602921fd1dba64e
Francesco Randi <francesco.randi@gmail.com>
2019-09-06 13:02:07 -0400

    removing _dsmm_hybrid from imports

--- wormbrain/reg/__init__.py
+++ wormbrain/reg/__init__.py
@@ -1,5 +1,5 @@
 from .reg import register
-from ._dsmm_hybrid import dsmm, _dsmm, _dsmm_parallel_wrapper, var_dict
+#from ._dsmm_hybrid import dsmm, _dsmm, _dsmm_parallel_wrapper, var_dict
 from ._dsmm_fullpy import _dsmm_fullpy
 from ._dsmm_c import _dsmmc_bare
 from ._dsmm_c_py import *
