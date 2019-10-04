dc89e2f3b39788b815916f4a20f7bd36bb12479b
David Turner <dmturner@princeton.edu>
2019-10-04 10:04:45 -0400

    Custom build extension for importing numpy
    setup.py imports numpy which means its required for install.
    We cannot just put it in install_requires though because the
    import numpy happens before the call to setup(). Instead, we
    register a custom build extension that imports numpy before
    and gets the include directory before building the extenstion.
    All of this gets invoked after dependencies get installed. This
    allows us to install with more modern and standard pip:
    
    pip install .

--- setup.py
+++ setup.py
@@ -1,22 +1,41 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
-import numpy
-
-_dsmm_c = Extension('wormbrain.reg._dsmm_c',
-                    sources = ['dsmm/dsmm.cpp','dsmm/dsmm_utils.cpp','wormbrain/reg/_dsmm_c.cpp'],
-                    include_dirs = [
-                        'dsmm',
-                        numpy.get_include()
-                        ],
-                    extra_compile_args=['-ffast-math','-O3'])#
+from distutils.command.build_ext import build_ext
 
 
-setup(name='wormbrain',
-      version='0.1',
-      description='Handling small brains',
-      author='Francesco Randi',
-      author_email='francesco.randi@gmail.com',
-      packages=['wormbrain','wormbrain.reg'],
-      ext_modules = [_dsmm_c]
-     )
+class CustomBuildExtCommand(build_ext):
+    """build_ext command for use when numpy headers are needed."""
+
+    def run(self):
+
+        # Import numpy here, only when headers are needed
+        import numpy
+
+        # Add numpy headers to include_dirs
+        self.include_dirs.append(numpy.get_include())
+
+        # Call original build_ext command
+        build_ext.run(self)
+
+
+ext_modules = [
+    Extension(
+        "wormbrain.reg._dsmm_c",
+        sources=["dsmm/dsmm.cpp", "dsmm/dsmm_utils.cpp", "wormbrain/reg/_dsmm_c.cpp"],
+        include_dirs=["dsmm"],
+        extra_compile_args=["-ffast-math", "-O3"],
+    )
+]
+
+setup(
+    name="wormbrain",
+    version="0.1",
+    description="Handling small brains",
+    author="Francesco Randi",
+    author_email="francesco.randi@gmail.com",
+    packages=["wormbrain", "wormbrain.reg"],
+    cmdclass = {'build_ext': CustomBuildExtCommand},
+    install_requires=["numpy"],
+    ext_modules=ext_modules,
+)
